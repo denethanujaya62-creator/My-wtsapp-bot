a6563d23a7cc6b121f58d2c6e3097aa1865d24c5
denethanujaya62-creator <denethanujaya62@gmail.com>
2026-07-05 16:04:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const {
-  default: browser: ['Chrome', 'Chrome', '144.0.0.0'],
-  useMultiFileAuthState,
-  DisconnectReason
+  default: makeWASocket,
+  useMultiFileAuthState
 } = require('@whiskeysockets/baileys');
 
 async function connectToWhatsApp() {
   const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
   
-  const sock = if (!sock.authState.creds.registered) { const phoneNumber = "94756086474"; const code = await sock.requestPairingCode(phoneNumber); console.log("Pairing Code: " + code); }
+  const sock = makeWASocket({
     auth: state,
-    printQRInTerminal: false
+    browser: ['Chrome', 'Chrome', '144.0.0.0']
   });
 
   sock.ev.on('creds.update', saveCreds);
   
   sock.ev.on('connection.update', (update) => {
-    const { connection, lastDisconnect } = update;
+    const { connection } = update;
     if (connection === 'close') {
       connectToWhatsApp();
     } else if (connection === 'open') {
